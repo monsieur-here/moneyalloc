16cfe2a9b907a2a268e8bf1c0b2e76fd6136bc8b
Siddharth Dyavanapalli <dyavanapallisiddharth784@gmail.com>
2022-05-15 11:18:20 +0530

    Using BootStrap and adding react props

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
+import Header from './BootStrap/Header';
 
 class App extends Component {
   render() {
     return (
-      <div className = "App">Good morning Intern</div>
+      <div className = "App">
+        <Header>MoneyAlloc</Header>
+      </div>
     )
   }
 }
